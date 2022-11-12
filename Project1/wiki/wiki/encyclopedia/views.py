73116f8d834dbd8efa4f937d13ff5270b65b4f32
DerickRedDanger <101509965+DerickRedDanger@users.noreply.github.com>
2022-11-11 22:14:06 -0300

    Complete up to search

--- Project1/wiki/wiki/encyclopedia/views.py
+++ Project1/wiki/wiki/encyclopedia/views.py
@@ -8,3 +8,34 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def page(request,title):
+    titles = util.list_entries()
+    if title in titles:
+        body=util.get_entry(title)
+        return render(request, "encyclopedia/page.html",{
+            "title":title, "body":body
+    })
+    else:
+        return render(request, "encyclopedia/notF.html",{
+            "title":title,
+            })
+def search(request):
+    print("test1")
+    print(request)
+    print("test2")
+    title=request.GET["q"]
+    titles = util.list_entries()
+    if title in titles:
+        body=util.get_entry(title)
+        return render(request, "encyclopedia/page.html",{
+            "title":title, "body":body
+        })
+    else:
+        results=[]
+        for i in titles:
+            if title.lower() in i.lower():
+                results.append(i)
+        return render(request, "encyclopedia/search.html", {
+            "entries": results, "title":title
+        })
+        
